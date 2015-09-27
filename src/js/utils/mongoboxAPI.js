26a58744ce7adf8761738ceaad4162465ab79603
Khalid Alharbi <kalharbi@users.noreply.github.com>
2015-09-27 12:13:01 -0600

    add timeout

--- src/js/utils/mongoboxAPI.js
+++ src/js/utils/mongoboxAPI.js
@@ -1,20 +1,20 @@
 import * as axios from "axios";
 import { appConstants } from "./constants";
 
-const rootUrl = "http://" + appConstants.SERVER_HOST + ":" + appConstants.SERVER_PORT +
-  "/" + appConstants.SERVER_END_POINT;
+const rootUrl = appConstants.SERVER_HOST + "/" + appConstants.SERVER_END_POINT;
 
 export function find(queryText) {
   var params = {
     q: queryText
   };
   return axios.get(rootUrl, {
-      params
+      params, timeout: 60000
     })
     .then(response => {
       return response.data;
     })
     .catch(err => {
+      console.error(err);
       return "Request failed.";
     });
 }
